783f225aee03ea77568b12af932e6489d81415dc
Zeng Luyang <zengluyang@gmail.com>
2015-03-25 02:15:53 -0700

    MOD:ctp

--- proc_ctp_log.py
+++ proc_ctp_log.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-f = open("log_ctp_23624.txt", "r")
+f = open("log_ctp_200885.txt", "r")
 energy = dict()
 for i in range(1,52):
 	energy[i]=3000
@@ -16,8 +16,9 @@ for line in f:
 		send_or_recv=data[4]
 		#print send_or_recv
 		if(send_or_recv=="SEND_CNT" or send_or_recv=="RECV_CNT"):
-			energy[id]=energy[id]-1
+			energy[id]=energy[id]-2
 			print time,id,energy[id]
 	#print time,id,type
+
 
 
